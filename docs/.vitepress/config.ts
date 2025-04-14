ed10c33e8e4604005d262ac1048e914feb9b1903
zhaojisen <1301338853@qq.com>
2025-04-14 14:13:18 +0800

    Fix static assets path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,46 +1,46 @@
-import nav from './nav';
-import { defineConfig } from 'vitepress';
+import nav from "./nav";
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
   title: "森弟儿的笔记空间",
   description: "我的个人笔记",
-  base: '/',
+  base: "/notes/",
   head: [
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
     [
-      'link',
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com' }
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
     ],
     [
-      'link',
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+        rel: "stylesheet",
+      },
     ],
-    [
-      'link',
-      { href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap', rel: 'stylesheet' }
-    ]
   ],
   themeConfig: {
     ...nav,
-    logo: '/logo.jpg',
-    outline: 'deep',
+    logo: "/logo.jpg",
+    outline: "deep",
     search: {
-      provider: 'local',
+      provider: "local",
       options: {
         translations: {
           button: {
-            buttonText: '搜索文档',
-            buttonAriaLabel: '搜索文档'
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
           },
           modal: {
-            noResultsText: '无法找到相关结果',
-            resetButtonTitle: '清除查询条件',
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
             footer: {
-              selectText: '选择',
-              navigateText: '切换',
-            }
-          }
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
         },
-      }
-    }
-  } 
-})
+      },
+    },
+  },
+});
